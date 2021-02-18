f781c5d5c8fc11f496834b657e1be46d753b629d
Shtirlits1972 <shtirlits1972@gmail.com>
2021-02-18 18:16:08 +0200

    font

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'Ut.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'визитка-2',
+      title: 'vizitka-2',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: BaseLayout(),
@@ -23,116 +21,121 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-class BaseLayout extends StatelessWidget{
+class BaseLayout extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: 
-       Align(alignment: Alignment.center, 
-      child: Container(
-        width: 600,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          image: DecorationImage(
-            image: AssetImage("assets/image/kapli2.jpg"),
-            fit: BoxFit.fill,
-          
-          ),
-        ),
-        child: Column(
-          
-          children: <Widget> [
-            Padding(padding: EdgeInsets.all(20),
-                      child: Container(
-
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 450,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              image: DecorationImage(
+                image: AssetImage(Ut.getWallpaper()),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(450),
+                      border: Border.all(
+                        color: Colors.yellow,
+                        width: 5.0,
+                      ),
+                      color: Colors.black,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(250.0),
+                      child: Image.asset(
+                        Ut.getAvatar(),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 260,
+                  margin: const EdgeInsets.only(bottom: 20),
                   decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(15),
-      border: Border.all(color: Colors.yellow, 
-    
-    width: 5.0, 
-    ),
-      color: Colors.black,
-  ),
-
-            child: ClipRRect( 
-              borderRadius: BorderRadius.circular(10.0),
-
-            child: Image.asset('assets/image/alf1.jpg', ),),
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.yellow,
+                      width: 5.0,
+                    ),
+                    color: Colors.black,
+                  ),
+                  child: RichText(
+                    textDirection: TextDirection.ltr,
+                    text: TextSpan(
+                        children: [
+                          WidgetSpan(
+                            child: Icon(
+                              Icons.account_box_rounded,
+                              size: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "      (063)150-00-88       ",
+                            style: GoogleFonts.montserrat(
+                                textStyle:
+                                    Theme.of(context).textTheme.headline6,
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.white),
+                          ),
+                        ],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        )),
+                  ),
+                ),
+                Container(
+                  width: 260,
+                  margin: const EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.yellow,
+                      width: 5.0,
+                    ),
+                    color: Colors.black,
+                  ),
+                  child: RichText(
+                    textDirection: TextDirection.ltr,
+                    text: TextSpan(
+                        children: [
+                          WidgetSpan(
+                            child: Icon(
+                              Icons.mail,
+                              size: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " info@sourceit.com.ua ",
+                          ),
+                        ],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        )),
+                  ),
+                ),
+              ],
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              textDirection: TextDirection.ltr,
+            ),
           ),
-        ),
- Container(
-   width: 260,
- margin: const EdgeInsets.only(bottom: 20),
-      decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(10),
-      border: Border.all(color: Colors.yellow, 
-    
-    width: 5.0, 
-    ),
-      color: Colors.black,
-  ),
-
-  child: 
-            RichText(
-                  textDirection: TextDirection.ltr,
-                    text: TextSpan(
-
-                      children: [
-                          WidgetSpan(
-                            child: Icon(Icons.account_box_rounded, size: 24, color: Colors.white,),
-                          ),
-                          TextSpan(text: "      (063)150-00-88       ",
-                          
-                           ),
-                      ],
-                      
-                      style: TextStyle(color: Colors.white, fontSize: 20, 
-                  )
-                 ),
-            ),),
-
-             Container(
-            width: 260,
-         margin: const EdgeInsets.only(bottom: 20),
-      decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(10),
-      border: Border.all(color: Colors.yellow, 
-    
-    width: 5.0, 
-    ),
-      color: Colors.black,
-  ),
-
-  child: 
-
-            RichText(
-                  textDirection: TextDirection.ltr,
-                    text: TextSpan(
-
-                      children: [
-                          WidgetSpan(
-                            child: Icon(Icons.mail, size: 24, color: Colors.white,),
-                          ),
-                          TextSpan(text: " info@sourceit.com.ua ",
-                          
-                           ),
-                      ],
-                      
-                      style: TextStyle(color: Colors.white, fontSize: 20,
-                  )
-                 ),
-            ),),
-
-        ],
-               crossAxisAlignment: CrossAxisAlignment.center,
-               mainAxisAlignment: MainAxisAlignment.center,
-               textDirection: TextDirection.ltr,
-               
         ),
       ),
-    ),
     );
   }
 }
