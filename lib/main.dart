2e4e3737b2438e7b853270b909e58ab5f709325d
Shtirlits1972 <shtirlits1972@gmail.com>
2021-02-20 21:52:51 +0200

    vizitka_20_2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'vizitka-2',
+      title: 'vizitka-20-1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
